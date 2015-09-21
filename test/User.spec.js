7320ac4f5c171a00c08b07394d2a130f823fb974
Michael K. Squires <sqmk@php.net>
2015-09-20 22:43:38 -0500

    GitHub #1 - Improve user tests

--- test/User.spec.js
+++ test/User.spec.js
@@ -4,13 +4,13 @@ let expect = require('chai').expect;
 let User   = require('../lib/User');
 
 describe('User', () => {
-  describe('constructor', () => {
-    beforeEach(() => {
-      this.id     = '123456789012345678901234567890';
-      this.device = 'test-device';
-      this.user   = new User(this.id, this.device);
-    });
+  beforeEach(() => {
+    this.id     = '123456789012345678901234567890';
+    this.device = 'test-device';
+    this.user   = new User(this.id, this.device);
+  });
 
+  describe('constructor', () => {
     it('should have default properties', () => {
       expect(this.user).to.have.all.keys('properties');
     });
@@ -21,6 +21,50 @@ describe('User', () => {
 
     it('should have set initial device', () => {
       expect(this.user.device).equal(this.device);
+    });
+  });
+
+  describe('validateUser', () => {
+    it('should not throw exception on User object', () => {
+      expect(() => {
+        User.validateUser(this.user);
+      }).to.not.throw(Error);
+    });
+
+    it('should throw exception on invalid value', () => {
+      expect(() => {
+        User.validateUser('invalid');
+      }).to.throw(Error);
+    });
+  });
+
+  describe('get/set id', () => {
+    it('should set id', () => {
+      let id       = 'abcdefghijabcdefghijabcdefghij';
+      this.user.id = id;
+
+      expect(this.user.id).equals(id);
+    });
+
+    it('should throw exception on invalid value', () => {
+      expect(() => {
+        this.user.id = 'invalid';
+      }).to.throw(Error);
+    });
+  });
+
+  describe('get/set device', () => {
+    it('should set device', () => {
+      let device       = 'test-device';
+      this.user.device = device;
+
+      expect(this.user.device).equals(device);
+    });
+
+    it('should throw exception on invalid value', () => {
+      expect(() => {
+        this.user.device = '.invalid.';
+      }).to.throw(Error);
     });
   });
 });
